26fa7c17f52ddabfaead52232e73ffbeb9178567
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-28 10:04:48 +0700

    feat: implement swagger

--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -1,12 +1,23 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Học sinh')
 @Controller('student')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
+  @ApiResponse({ 
+    status: HttpStatus.CREATED, 
+    description: 'Tạo học sinh thành công'
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Email hoặc mật khẩu đã tồn tại',
+  })
+  @ApiOperation({ summary: 'Tạo học sinh mới(Admin only)'})
   @Post('create')
   async createStudent(@Body() createStudentDto: CreateStudentDto) {
     return await this.studentService.createStudent(createStudentDto);
