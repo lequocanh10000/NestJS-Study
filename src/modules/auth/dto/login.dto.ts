d247d05b21eb4c915d20b0446cd3e0aaa8357131
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-28 14:03:40 +0700

    feat: create login dto

---
+++ src/modules/auth/dto/login.dto.ts
@@ -0,0 +1,10 @@
+import { StringRequired } from "src/common/decorators";
+
+export class LoginDto {
+
+    @StringRequired('Email')
+    email: string;
+
+    @StringRequired('Mật khẫu')
+    password: string;
+}
