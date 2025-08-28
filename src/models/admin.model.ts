ff2a551eb14125af5b3e645667a659e780263a43
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-28 14:03:25 +0700

    feat: create admin dto files

--- src/models/admin.model.ts
+++ src/models/admin.model.ts
@@ -1,4 +1,5 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { BeforeValidate, Column, DataType, Model, Table } from "sequelize-typescript";
+import * as bcrypt from 'bcryptjs';
 
 @Table
 export class Admin extends Model<Admin> {
@@ -14,4 +15,23 @@ export class Admin extends Model<Admin> {
         type: DataType.STRING,
     })
     password: string;
+
+    comparePassword(password: string) {
+        const {password: passwordInDb} = this.get({plain: true});
+        return bcrypt.compareSync(password, passwordInDb);
+    }
+
+    getAdminWithoutPassword() {
+        const {password: _, ...rest} = this.get({plain: true});
+        return rest;
+    }
+
+    @BeforeValidate
+    static hashPassword(admin: Admin) {
+        if(admin.isNewRecord) {
+            const password = admin.get('password');
+            const hashedPassword = bcrypt.hashSync(password, 10);
+            admin.setDataValue('password', hashedPassword); 
+        }
+    }
 }
