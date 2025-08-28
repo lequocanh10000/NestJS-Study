e16060a81ccd90ae4b82066263c9b6c9e09c89a9
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-28 10:04:59 +0700

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/response.interceptor';
 import { AllExceptionFilter } from './common/filter/all-expection.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('bootstrap');
   const configService = new ConfigService();
+
+  app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
     new ValidationPipe(
@@ -20,12 +23,21 @@ async function bootstrap() {
     ));
   
   app.useGlobalInterceptors(new TransformInterceptor);
-
   app.useGlobalFilters(new AllExceptionFilter);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Study API')
+    .setDescription('Build API for Study Management API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, documentFactory);
 
   const port = configService.get<string>('PORT') || 4002;
 
   logger.log(`Server starts on port ${port}`);
+  logger.log(`Swagger running on http://localhost:${port}/api/v1/docs`);
   await app.listen(port);
 }
 bootstrap();
